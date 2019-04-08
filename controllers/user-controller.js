f562dbdc694812294fffccd853807d3a32dc9841
Namita Shenai <namita.shenai@gmail.com>
2019-04-08 15:51:44 -0700

    Added blank folder & file for Controllers

---
+++ controllers/user-controller.js
@@ -0,0 +1,5 @@
+var db = require("../models");
+
+//defining methods for user controller/user routes
+module.exports = {
+}
--- schema.sql
+++ schema.sql
@@ -3,33 +3,4 @@ DROP DATABASE IF EXISTS codepals_db;
 --Create Database structure for sequelize to create models within it. 
 CREATE DATABASE codepals_db; 
 
-
-
-
-
-Executing (default): CREATE TABLE IF NOT EXISTS `usersActivities`
- (`userId` INTEGER NOT NULL , `activityId` INTEGER NOT NULL ,
-  `interested` TINYINT(1) DEFAULT false, `createdAt` DATETIME NOT NULL, 
-  `updatedAt` DATETIME NOT NULL, 
-  UNIQUE `usersActivities_userId_activityId_unique` (`userId`, `activityId`), 
-  PRIMARY KEY (`userId`, `activityId`),
-   FOREIGN KEY (`userId`) REFERENCES `users` (`id`) 
-   ON DELETE CASCADE ON UPDATE CASCADE, 
-   FOREIGN KEY (`activityId`) REFERENCES `activities` (`id`) 
-   ON DELETE CASCADE ON UPDATE CASCADE) ENGINE=InnoDB;
-
-
-
-
-Executing (default): CREATE TABLE IF NOT EXISTS `usersSkills`
- (`userId` INTEGER NOT NULL , `skillId` INTEGER NOT NULL , 
- `hasSkill` TINYINT(1) DEFAULT false, `createdAt` DATETIME NOT NULL, 
- `updatedAt` DATETIME NOT NULL,
-  UNIQUE `usersSkills_userId_skillId_unique` (`userId`, `skillId`), 
-  PRIMARY KEY (`userId`, `skillId`), 
-  FOREIGN KEY (`userId`) REFERENCES `users` (`id`) 
-  ON DELETE CASCADE ON UPDATE CASCADE, 
-  FOREIGN KEY (`skillId`) REFERENCES `skills` (`id`) 
-  ON DELETE CASCADE ON UPDATE CASCADE) ENGINE=InnoDB;
-
-
+USE codepals_db;
