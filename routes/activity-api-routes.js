357ad604306214657d7fc3abafb13ccef6934aeb
Susie <45341415+Haldevel@users.noreply.github.com>
2019-04-13 14:24:50 -0700

    clean up activity related files and added some comments

--- routes/activity-api-routes.js
+++ routes/activity-api-routes.js
@@ -18,7 +18,6 @@ module.exports = function (app) {
         })
     });
 
-    //we already have the dashboard route inside /api/user - how we should organize it?
     //Get Activity Data for the logged in user 
     app.get('/dashboard', function (req, res) {
 
@@ -62,20 +61,18 @@ module.exports = function (app) {
     });
 
 
-    //the get request for adding a new activity page
+    //The get request for adding a new activity page - it will render addactivity.handlebars page
     app.get('/addactivity', function (req, res) {
         if (req.isAuthenticated()) {
-            console.log("The user is authenticated");
-            //console.log(req.session.passport.user);
-            //var usId = req.session.passport.user.id;
+            console.log("The user is authenticated"); //we authenticated the user
 
+            //we will select all active current users to populate drop-down menu in our addactivity form
             db.users.findAll({ where: { active: 1 } }).then(function (dbUsers) {
-                console.log(dbUsers);
+                //console.log(dbUsers);
                 var hbsObject = {
                     users: dbUsers
                 };
-                console.log(hbsObject);
-                res.render("addactivity", hbsObject);
+                res.render("addactivity", hbsObject); //render the form
             })
         }
         else {
@@ -91,56 +88,53 @@ module.exports = function (app) {
     app.get('/updactivity/:id', function (req, res) {
         /*  if (req.isAuthenticated()) { */
         console.log("The user is authenticated");
-        //const Op = Sequelize.Op;
         var id = req.params.id;
-        //console.log(req.session.passport.user);
-        //var usId = req.session.passport.user.id;
         //pass each activity via an id
-        //this object will contain all the data we want
+        //this object will contain all the data we want to display on update activity page
+        //we will create one big object containg two arrays of objects:
+        //first includes all the activity data plus all participating users, and another 
+        //includes the users who was not invited yet
         var addedUsers = [];
         var hbsCurrentUsers = {
             activityUsers: [],
             allUsers: []
         };
 
+        //in the first call we want to get the activity with the corresponding id 
+        //and all the users participating in the activity
         db.activities.findAll({
             where: { id: id },
             include: [{ model: db.users, as: "users" }]
         }).then(function (dbActivity) {
-            console.log("inside first promise");
+
+            //we constract the first part of the complex object 
             hbsCurrentUsers.activityUsers = dbActivity;
 
-            //var obj = JSON.parse(dbActivity);
-
+            //this array will be used below to filter the users who were not invited
+            //using notIN clause of sequelize ORM 
             var addedUsers = dbActivity[0].users;
-            console.log("addedUsers " + addedUsers.length);
-            var addeduserIds = [];
+            var addeduserIds = [];  //the array contains ids of the invited users
             for (var i = 0; i < addedUsers.length; i++) {
                 addeduserIds.push(addedUsers[i].id);
             }
-            console.log("ids array" + addeduserIds);
 
-
-            db.users.findAll({
-                //Dont include logged in user 
-                //using the not operator of sequlize i.e example: userid NOT "1                   
+            db.users.findAll({                
                 where: {
                     id: { [Op.notIn]: addeduserIds }
                 }
             }).then(function (dbUsers) {
-                console.log("inside second promise");
+
                 //filter out the user who were not invited yet
                 console.log("currentUsers " + addedUsers.length);
                 //accessing allUsers and filtering out the users not invited yet
                 var all = dbUsers;
-                console.log("All Users " + all.length);
 
                 hbsCurrentUsers.allUsers = dbUsers;
-                //SEND JSON TO TEST
-                //res.json(hbsCurrentUsers);
+                //res.json(hbsCurrentUsers);  //this line was used for testing
+                //render update activity page and send the object containg two arrays of objects:
+                //first including all the activity data and all participationg users, and another 
+                //to include all the users who were not invited
                 res.render("updactivity", hbsCurrentUsers);
-                //populate the handlebars object with the current users for the current activity
-
             })
 
             /* }
