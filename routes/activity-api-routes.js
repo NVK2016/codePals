ef888dd930c9d65e5bf3baf739313419b7278413
Susie <45341415+Haldevel@users.noreply.github.com>
2019-04-13 17:47:49 -0700

    resolved conflict in activity-api-routes

--- routes/activity-api-routes.js
+++ routes/activity-api-routes.js
@@ -19,6 +19,9 @@ module.exports = function (app) {
         })
     });
 
+    //******HALINA PART 1 END*********************************//
+
+    //*****SAM BEGIN******************************************//
     //Get Activity Data for the logged in user 
     app.get('/dashboard', function (req, res) {
 
@@ -83,7 +86,9 @@ module.exports = function (app) {
         };
     });
 
+    //*****SAM END******************************************//
 
+    //******HALINA PART 2 BEGIN*****************************//
     //The get request for adding a new activity page - it will render addactivity.handlebars page
     app.get('/addactivity', function (req, res) {
         if (req.isAuthenticated()) {
@@ -165,105 +170,83 @@ module.exports = function (app) {
             } */
         });
 
+    });
 
+    //Add a new Project / Meetup  
+    app.post('/addactivity', function (req, res) {
 
+        if (req.isAuthenticated()) {
+            console.log("The user is authenticated");
+            console.log(req.session.passport.user);
+            var usId = req.session.passport.user.id;
 
-        //Add a new Project / Meetup  
-        app.post('/addactivity', function (req, res) {
+            console.log(req.body.activity);
+            var obj = JSON.parse(req.body.activity); //getting an object from json 
+            console.log(obj);
+            //var leaderId = passedActivity.adminId;   //will use if passport works!!!!!!
+            var leaderId = usId; // will have to grab it from req
+            //pass new values to the activities table model to create a new record
+            //in the activities table and in the same transaction to add multiple records to the join usersActivities table
 
-            if (req.isAuthenticated()) {
-                console.log("The user is authenticated");
-                console.log(req.session.passport.user);
-                var usId = req.session.passport.user.id;
+            var arrayIds = obj["participantsIds"];
+            //hardcoding adminId until passport is working
 
-                console.log(req.body.activity);
-                var obj = JSON.parse(req.body.activity); //getting an object from json 
-                console.log(obj);
-                //var leaderId = passedActivity.adminId;   //will use if passport works!!!!!!
-                var leaderId = usId; // will have to grab it from req
-                //pass new values to the activities table model to create a new record
-                //in the activities table and in the same transaction to add multiple records to the join usersActivities table
+            console.log('Creating a new activity!');
+            db.activities.create({
+                adminId: leaderId,
+                title: obj.title,
+                description: obj.description,
+                location: obj.location,
+                estimateStartDate: obj.estimateStartDate,
+                actType: obj.actType,
+                active: true,
+            }).then(function (dbActivity) {
+                //the array will hold objects representing usersActivities table
+                var allInvited = [];
 
-                var arrayIds = obj["participantsIds"];
-                //hardcoding adminId until passport is working
-
-                console.log('Creating a new activity!');
-                db.activities.create({
-                    adminId: leaderId,
-                    title: obj.title,
-                    description: obj.description,
-                    location: obj.location,
-                    estimateStartDate: obj.estimateStartDate,
-                    actType: obj.actType,
-                    active: true,
-                }).then(function (dbActivity) {
-                    //the array will hold objects representing usersActivities table
-                    var allInvited = [];
-
-                    //create the corresponding usersActivities objects based on the ids in the arrayIds
-                    for (var i = 0; i < arrayIds.length; i++) {
-                        var inviteeObj = {
-                            userId: parseInt(arrayIds[i]),
-                            activityId: dbActivity.dataValues.id,
-                            interested: false
-                        };
-                        allInvited.push(inviteeObj);
-                    }
-
-                    //create the usersActivities object for the admin and push it to the array
-                    var adminObj = {
-                        userId: leaderId,
+                //create the corresponding usersActivities objects based on the ids in the arrayIds
+                for (var i = 0; i < arrayIds.length; i++) {
+                    var inviteeObj = {
+                        userId: parseInt(arrayIds[i]),
                         activityId: dbActivity.dataValues.id,
-                        interested: true
+                        interested: false
                     };
-                    allInvited.push(adminObj);
+                    allInvited.push(inviteeObj);
+                }
 
-                    //insert multiple records from the array to the usersActivities table
-                    db.usersActivities.bulkCreate(allInvited, {
-                        returning: true
-                    }).then(function (dbUsersActivities) {
-                        console.log("Activity added " + dbUsersActivities.dataValues);
-                        res.json(dbUsersActivities);
-                        //will have to redirect to a dashboard for the user
-                    })
-<<<<<<< HEAD
-                        .catch(function (err) {
-                            console.log(err);
-                            res.json(error);
-                        });
-=======
+                //create the usersActivities object for the admin and push it to the array
+                var adminObj = {
+                    userId: leaderId,
+                    activityId: dbActivity.dataValues.id,
+                    interested: true
+                };
+                allInvited.push(adminObj);
 
-
->>>>>>> bff24e21070aa35e533136fac610dd1ee5dba674
+                //insert multiple records from the array to the usersActivities table
+                db.usersActivities.bulkCreate(allInvited, {
+                    returning: true
+                }).then(function (dbUsersActivities) {
+                    console.log("Activity added " + dbUsersActivities.dataValues);
+                    res.json(dbUsersActivities);
+                    //will have to redirect to a dashboard for the user
                 })
-            }
-            else {
-                console.log("auth", req.isAuthenticated());
-                res.redirect("/");
-            }
+                    .catch(function (err) {
+                        console.log(err);
+                        res.json(error);
+                    });
+            })
+        }
+        else {
+            console.log("auth", req.isAuthenticated());
+            res.redirect("/");
+        }
+    });
 
+    //Updates the activity with the member values 
+    app.put('/updactivity', function (req, res) {
 
-        });
+    });
 
+};
 
-        app.put("/updateinterest", function (req, res) {
-            db.usersActivities.update(
-                {
-                    interested: req.body.interested
-                }, {
-                    where: {
-                        userId: req.session.passport.user.id,
-                        activityId: req.body.activityId
-                    }
-                }).then(function (data) {
-                    res.redirect("/dashboard")
-                })
-        })
-
-        //Updates the activity with the member values 
-        app.put('/updactivity', function (req, res) {
-
-        });
-
-    };
-
+//******HALINA PART 2 END*****************************//
